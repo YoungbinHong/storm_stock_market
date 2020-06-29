de642f8a3b70ff70ca22fc4b80454fe9c9077802
archer0307 <code_archer@naver.com>
2020-06-29 21:43:42 +0900

    랜덤 함수 사용을 위한 스레드 생성

--- app/src/main/java/com/example/stormstockmarket/MainActivity.java
+++ app/src/main/java/com/example/stormstockmarket/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.stormstockmarket;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Random Thread
+        class NewRunnable implements Runnable {
+            @Override
+            public void run() {
+                while (true) {
+
+                    double randomDouble1 = Math.random();
+                    int randomInt1 = (int)(randomDouble1*100)+1;
+
+                    final TextView 단절 = (TextView) findViewById(R.id.단절통신현재가);
+                    단절.setText(randomInt1+"");
+
+                    try {
+                        Thread.sleep(5000) ;
+                    } catch (Exception e) {
+                        e.printStackTrace() ;
+                    }
+                }
+            }
+        }
+
+        NewRunnable nr = new NewRunnable() ;
+        Thread t = new Thread(nr) ;
+        t.start() ;
+
+
     }
+
 }
